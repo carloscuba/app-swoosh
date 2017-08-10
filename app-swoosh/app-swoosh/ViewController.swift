63c9a6531387a09e799873bd9e5744b328946070
Carlos Cuba <carlos.cuba@bonzzu.com>
2017-08-10 01:16:03 -0500

    view controller constraints added in storyboard

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var swoosh: UIImageView!
+    @IBOutlet var bgImg: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {

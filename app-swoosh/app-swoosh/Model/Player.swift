75941c43bdd371031e77df4f129b0f23e6589de7
Carlos Cuba <carlos.cuba@bonzzu.com>
2017-08-10 02:34:52 -0500

    model created for player. IBOutlets and logic added for buttons on LeagueVC

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Carlos Cuba on 8/10/17.
+//  Copyright © 2017 Carlos Cuba. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague:String?
+    var selectedSkillLevel:String?
+}
